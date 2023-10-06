6fd23ad9f75c3082d04a7c6a31618692f70a1495
Matheus Fidelis <matheus@nanoshots.com.br>
2023-10-05 23:49:07 -0300

    spring gateway instrumentation

--- gateway/src/main/java/com/books/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/books/gateway/GatewayApplication.java
@@ -1,13 +1,31 @@
 package com.books.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
+
+	@Value("${books.endpoint}")
+	private String booksURI;
+
+	@Value("${reviews.endpoint}")
+	private String reviewsURI;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator routers(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/books/**").filters(f -> f.prefixPath("/")).uri(booksURI))
+				.route(r -> r.path("/author/**").filters(f -> f.prefixPath("/")).uri(booksURI))
+				.route(r -> r.path("/reviews/**").filters(f -> f.prefixPath("/")).uri(reviewsURI))
+				.build();
+	}
 }
